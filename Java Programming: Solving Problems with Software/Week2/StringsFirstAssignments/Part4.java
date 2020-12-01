562084b836024527c5199c849a6f81dac296d62e
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-12-01 19:02:23 +0200

    Update Part4.java

--- Java Programming: Solving Problems with Software/Week2/StringsFirstAssignments/Part4.java
+++ Java Programming: Solving Problems with Software/Week2/StringsFirstAssignments/Part4.java
@@ -4,17 +4,20 @@ import edu.duke.*;
 public class Part4 {
     
   public void readUrl() {
+    // Using URLResource we scan a given link and we create an object
     URLResource links = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
-    String toSearch = "youtube";
     String result = "";
+    // We iterate through the lines in the wabpage
     for (String link : links.lines()) {
+        // We check if the word 'youtube' is present in the link
         int index = link.toLowerCase().indexOf("youtube");
         if (index != -1) {
+            // We extract the whole url
             String quote = "\"";
             int firstQuote = link.indexOf(quote);
             int lastQuote = link.lastIndexOf(quote);
             result = link.substring(firstQuote, lastQuote + 1);
-         
+            // Print the urls
             System.out.println(result);
         }
     }
