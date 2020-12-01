a003cb7c113ac75cf1ca6c9a98dec83e5fc5babc
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-12-01 11:17:27 +0200

    Update Part1.java

--- Java Programming: Solving Problems with Software/Week2/Part1/Part1.java
+++ Java Programming: Solving Problems with Software/Week2/Part1/Part1.java
@@ -1,14 +1,19 @@
 public class Part1 {
     public String findSimpleGene(String dna) {
+        // Initialize an empty string result
         String result = "";
+        // Find the beginning of the gene in the dna string
         int geneStart = dna.indexOf("ATG");
+        // If we can't find 'ATG' in the dna string, we know there's no gene present in that string
         if (geneStart == -1) {
             return "Invalid Gene Start";
         }
+         // If we can't find 'TAA' in the dna string, we know there's no gene present in that string
         int geneEnd = dna.indexOf("TAA", geneStart + 3);
         if (geneEnd == -1) {
             return "Invalid Gene End";
         }
+        // We check if the gene has a valid length
         result = dna.substring (geneStart, geneEnd + 3);
         int resultLength = result.length();
         if (resultLength %  3 != 0) {
@@ -18,8 +23,11 @@ public class Part1 {
       }
 
     public void testsFindSimpleGene() {
+        // Create an array of dna strings for testing our code
         String[] test = {"ATGATATAA", "TAGGCATT", "AATGTGACAGTAA", "TTAGTTGCGACTACATT", "CGATGATGGGCATAAA"};
+        // Iterate through the strings in the array
         for (int i = 0; i < test.length; i++) {
+            // Return a result string for each dna string
             String result = findSimpleGene(test[i]);
             System.out.println(result);
         }
