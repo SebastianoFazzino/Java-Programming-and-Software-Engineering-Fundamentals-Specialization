a2dda72cd25d2c377091bac355a9db2f342189e5
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-12-09 13:55:41 +0200

    Update LowestHumidity.java

--- Java Programming: Solving Problems with Software/Week3/ParsingWeatherData/LowestHumidity.java
+++ Java Programming: Solving Problems with Software/Week3/ParsingWeatherData/LowestHumidity.java
@@ -1,4 +1,3 @@
-
 import edu.duke.*;
 import org.apache.commons.csv.*;
 import java.io.*;
@@ -9,12 +8,13 @@ public class LowestHumidity {
         // This method finds the lowest humidity value in a given day
         CSVRecord lowestSoFar = null;
         for (CSVRecord currentRow : parser) {
-            if ( lowestSoFar == null && currentRow.get("Humidity") != "N/A" ) {
+            if (lowestSoFar == null) {
                 lowestSoFar = currentRow;
             }
             else {
-                if ( currentRow.get("Humidity") != "N/A" ) {
-                    double currentHum = Double.parseDouble(currentRow.get("Humidity"));
+                String humidity = currentRow.get("Humidity");
+                if (humidity.contains("N/A") == false) {
+                    double currentHum = Double.parseDouble(humidity);
                     double lowestHum = Double.parseDouble(lowestSoFar.get("Humidity"));
                     if (currentHum < lowestHum) {
                         lowestSoFar = currentRow;
